905563ffcdcc1ca8869c0f9c8757c5686dd3be82
前田達哉 <takeda.kiyosi@icloud.com>
2021-06-03 18:17:18 +0900

    受ける側と送る側で色を変えました。受ける側を出力しました。

--- chat/index.js
+++ chat/index.js
@@ -1,12 +1,18 @@
 let button = document.getElementById('button');
 const socket =new WebSocket('ws://10.1.234.2:8888');
-    
 
  // メッセージの待ち受け(GET的な)
 socket.addEventListener("message",(event)=>{
     const data=(JSON.parse(event.data));
     console.log(data);
+    const receive_message=document.querySelector("#receive-message");
+    receive_message.insertAdjacentHTML('afterbegin',
+    `<ul>
+    <li>${data.body}</li>
+    1</ul>`
+    );
 });
+
 
 button.addEventListener(`click`,()=>{
     let message_input=document.querySelector("#message_input").value;
@@ -19,13 +25,15 @@ button.addEventListener(`click`,()=>{
             body:message_input,
             to:'*',
             isBoard: true,
+            option:1
         }));
         
 
 
-    const send_message=message_list.innerHTML=`<ol>
+    const send_message=message_list.insertAdjacentHTML('afterbegin',`<ul>
     <li>${message_input}</li>
-</ol>`;
+</ul>`
+);
 ;
 })
 
